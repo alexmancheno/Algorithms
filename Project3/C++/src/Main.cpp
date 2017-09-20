276b47ec3cf1f7bd51afa8ba5154cc153b70c94b
alexmancheno <alex.mancheno@gmail.com>
2017-09-20 18:17:44 -0400

    Added Project3

---
+++ Project3/C++/src/Main.cpp
@@ -0,0 +1,282 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+struct listBinTreeNode
+{
+    string chStr;
+    string code;
+    int prob;
+    listBinTreeNode* next;
+    listBinTreeNode* left;
+    listBinTreeNode* right;
+
+    listBinTreeNode()
+    {
+        next = NULL;
+        prob = 0;
+        chStr = "";
+        next = NULL;
+        left = NULL;
+        right = NULL;
+    }
+
+    listBinTreeNode(string s, int p)
+    {
+        next = NULL;
+        chStr = s;
+        prob = p;
+        next = NULL;
+        left = NULL;
+        right = NULL;
+    }
+
+    static void printNode(listBinTreeNode* t) { cout << t->toString() << endl; }
+    
+    string toString()
+    {
+        // ---- Return the contents of this node ----
+        string s = "t = { 'chStr' : ";
+
+        // Concatenate current node's chStr and prob.
+        s += "'" + chStr + "', 'prob' : " + to_string(prob) + ", ";
+
+        // Concatenate next node's chStr.
+        s += "'next->chStr' : ";
+        if (this->next == NULL) s += "NULL, ";
+        else                    s += "'" + next->chStr + "', ";
+
+        // Concatenate left node's chStr.
+        s += "'left->chStr' : ";
+        if (this->left == NULL) s += "NULL, ";
+        else                    s += "'" + left->chStr + "', ";
+
+        // Concatenate right node's chStr.
+        s += "'right->chStr' : ";
+        if (this->right == NULL) s += "NULL }";
+        else                     s += "'" + right->chStr + "' }";
+
+        return s;
+    }
+};
+
+struct HuffmanLinkedList 
+{
+    listBinTreeNode* listHead;
+    listBinTreeNode* oldListHead;
+
+    HuffmanLinkedList() { listHead = new listBinTreeNode("dummy", 0); }
+
+    HuffmanLinkedList(listBinTreeNode* n) { listHead = n; }
+
+    void constructHuffmanList(ifstream& input, ofstream& output5)
+    {
+        string chStr;
+        int prob;
+
+        output5 << "constructHuffmanList(input, output5): \n";
+
+        while (input.good())
+        {
+            input >> chStr; // Read in character
+            input >> prob;  // Read in probability
+
+            // Create new node with char & prob.
+            listBinTreeNode* newNode = new listBinTreeNode(chStr, prob); 
+    
+            // Find correct spot and insert new node between spot and spot.next. 
+            listInsert(newNode->prob, newNode); 
+
+            // Print list to console and output file.
+            printList();  
+            output5 << toString() << endl;    
+        }
+        output5 << endl; // To seperate this output from the next algorithm step.
+    }
+
+    listBinTreeNode* findSpot(int prob)
+    {
+        listBinTreeNode* i = listHead;
+        while (i->next != NULL && prob > i->next->prob)
+            i = i->next;
+
+        return i;
+    }
+
+    void listInsert(int prob, listBinTreeNode* newNode)
+    {
+        listBinTreeNode* spot = findSpot(prob);
+        newNode->next = spot->next;
+        spot->next = newNode;
+    }
+
+    bool isEmpty() { return listHead->next == NULL; }
+
+    void printList()
+    {
+        cout << this->toString() << endl;
+    }
+
+    string toString()
+    {
+        string nextString;
+        string result = "listHead-->";
+        for (listBinTreeNode* i = this->listHead; i != NULL; i = i->next)
+        {
+            if (i->next == NULL) nextString = "NULL)-->";
+            else                 nextString = "\"" + i->next->chStr + "\")-->";
+            
+            result += "(\"" + i->chStr + "\", " + to_string(i->prob) + ", " + nextString;
+        }
+        result += "NULL";
+        return result;
+    }
+
+};
+
+struct HuffmanBinaryTree
+{
+    listBinTreeNode* root;
+
+    HuffmanBinaryTree() { root = NULL; }
+
+    void constructHuffmanBinTree(HuffmanLinkedList* list, ofstream& output5)
+    {
+        // Create a dummy node and oldListHead point to dummy node and save original linked list.
+        list->oldListHead = new listBinTreeNode("yummy", 0);
+        list->oldListHead->next = list->listHead->next;  
+        
+        // Move listhead from "dummy" node to first node in list.
+        list->listHead = list->listHead->next; 
+        listBinTreeNode* newNode;
+
+        output5 << "constructHuffmanBinTree(list, output5): \n";
+
+        // Keep going until there is one node in list. 
+        while (list->listHead->next != NULL) 
+        {
+            string combinedChar = list->listHead->chStr + list->listHead->next->chStr;
+            int combinedProb = list->listHead->prob + list->listHead->next->prob;
+
+            // Create newNode with combined prob & char
+            newNode = new listBinTreeNode(combinedChar, combinedProb); 
+
+            newNode->left = list->listHead; // Set newNode's left to first node of the list.
+            newNode->right = list->listHead->next; // Set newNode's right to second node of the list.
+
+            // Search and insert newNode starting from where listHead currently is.
+            list->listInsert(newNode->prob, newNode);   
+
+             // Print the new node for debugging.
+            listBinTreeNode::printNode(newNode);       
+            output5 << newNode->toString() << endl;
+
+            // Print the current list for debugging.
+            list->printList();                          
+            output5 << list->toString() << endl;
+
+            // Advance the current list head by two pointers.
+            list->listHead = list->listHead->next->next; 
+        }
+        // Set the root to the newly created HuffmanBinaryTree when there is only one node left.
+        root = newNode; 
+    }
+
+    void preOrder(listBinTreeNode* t, ofstream& output3)
+    {   
+        if (t != NULL)
+        {
+            listBinTreeNode::printNode(t);
+            output3 << t->toString() << endl;
+            preOrder(t->left, output3);
+            preOrder(t->right, output3);
+        }
+    }
+
+    void inOrder(listBinTreeNode* t, ofstream& output4)
+    {
+        if (t != NULL)
+        {
+            inOrder(t->left, output4);
+            listBinTreeNode::printNode(t);
+            output4 << t->toString() << endl;
+            inOrder(t->right, output4);
+        }
+    }
+
+    void postOrder(listBinTreeNode* t, ofstream& output5)
+    {
+        if (t != NULL)
+        {
+            postOrder(t->left, output5);
+            postOrder(t->right, output5);
+            listBinTreeNode::printNode(t);
+            output5 << t->toString() << endl;
+        }
+    }
+
+    static bool isLeaf(listBinTreeNode* t) { return (t->left == NULL && t->right == NULL); }
+
+    static void constructCharCode(listBinTreeNode* t, string code, ofstream& output1)
+    {
+        if (t == NULL)
+        { 
+            output1 << "This is an empty tree." << endl;
+            cout << "This is an empty tree." << endl;
+            exit(0); 
+        }
+        else if (HuffmanBinaryTree::isLeaf(t))
+        {
+            t->code = code;
+            output1 << t->chStr << " : " << t->code << endl;
+            cout << t->chStr << " : " << t->code << endl;
+        }
+        else
+        {
+            HuffmanBinaryTree::constructCharCode(t->left, code + "0", output1);
+            HuffmanBinaryTree::constructCharCode(t->right, code + "1", output1);
+        }
+    }
+};
+
+
+int main(int argc, char** argv)
+{
+    ifstream input;
+    ofstream output1, output2, output3, output4, output5;
+
+    input.open(argv[1]);    // Open input file.
+    output1.open(argv[2]);   // For Huffman pairs.
+    output2.open(argv[3]);   // For pre-order traversal of Huffman binary tree.
+    output3.open(argv[4]);   // For in-order traversal of Huffman binary tree.
+    output4.open(argv[5]);   // For post-order traversal of Huffman binary tree.
+    output5.open(argv[6]);   // For debugging outputs.
+
+    // Create Huffman linked list with input file.
+    HuffmanLinkedList* hll = new HuffmanLinkedList(); 
+    hll->constructHuffmanList(input, output5); 
+
+    // Create Huffman binary tree from linked list.
+    HuffmanBinaryTree* hbt = new HuffmanBinaryTree();
+    hbt->constructHuffmanBinTree(hll, output5);      
+
+    // Create the code for each char.
+    HuffmanBinaryTree::constructCharCode(hbt->root, "", output1);  
+
+    output2 << "preOrder: \n";
+    output3 << "inOrder: \n";
+    output4 << "postOrder: \n";
+
+    hbt->preOrder(hbt->root, output2); 
+    hbt->inOrder(hbt->root, output3);
+    hbt->postOrder(hbt->root, output4);
+
+    input.close();
+    output1.close();
+    output2.close();
+    output3.close();
+    output4.close();
+    output5.close();
+    return 0;
+}
--- Project3/Java/src/Main.java
+++ Project3/Java/src/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 class listBinTreeNode
 {
     public String chStr;
+    public String code;
     public int prob;
     public listBinTreeNode next;
     public listBinTreeNode left;
@@ -11,23 +12,51 @@ class listBinTreeNode
 
     public listBinTreeNode()
     {
-        chStr = null;
+        next = null;
         prob = 0;
+        chStr = "";
+        next = null;
+        left = null;
+        right = null;
     }
 
     public listBinTreeNode(String s, int p)
     {
+        next = null;
         chStr = s;
         prob = p;
+        next = null;
+        left = null;
+        right = null;
     }
 
-    // Needs to be finished.
-    public String printNode(listBinTreeNode t)
-    {
-        String m = "(" + chStr + " : " + prob + ")" ;
-        String n;
+    public static void printNode(listBinTreeNode t) {  System.out.println(t); }
 
-        return m; 
+    @Override
+    public String toString()
+    {
+        // ---- Return the contents of this node ----
+        String s = "t = { 'chStr' : ";
+
+        // Concatenate current node's chStr and prob.
+        s += "'" + chStr + "', 'prob' : " + prob + ", ";
+
+        // Concatenate next node's chStr.
+        s += "'next->chStr' : ";
+        if (next == null) s += "null, ";
+        else                    s += "'" + next.chStr + "', ";
+
+        // Concatenate left node's chStr.
+        s += "'left->chStr' : ";
+        if (left == null) s += "null, ";
+        else                    s += "'" + left.chStr + "', ";
+
+        // Concatenate right node's chStr.
+        s += "'right->chStr' : ";
+        if (right == null) s += "null }";
+        else                     s += "'" + right.chStr + "' }";
+
+        return s;
     }
 }
 
@@ -36,23 +65,45 @@ class HuffmanLinkedList
     public listBinTreeNode listHead;
     public listBinTreeNode oldListHead;
 
-    public HuffmanLinkedList()
-    {
-        listHead = new listBinTreeNode("dummy", 0);
-    }
+    public HuffmanLinkedList() { listHead = new listBinTreeNode("dummy", 0); }
 
-    public void constructHuffmanList()
+    public void constructHuffmanList(Scanner input, FileWriter output5)
     {
+        String chStr;
+        int prob;
 
+        try
+        {
+            output5.write("constructHuffmanList(intput, output5): \n");
+
+            while (input.hasNext())
+            {
+                chStr = input.next();  // Read in character
+                prob = input.nextInt();  // Read in probability
+
+                listBinTreeNode newNode = new listBinTreeNode(chStr, prob);
+
+                // Find correct spot and insert new node between spot and spot.next. 
+                listInsert(newNode.prob, newNode); 
+
+                // Print list to console and output file.
+                printList();
+                output5.write(this.toString());
+            }
+            output5.write('\n');
+        }
+        catch (Exception e)
+        {
+            Main.printError(e);
+        }
     }
 
     public listBinTreeNode findSpot(int prob)
     {
         listBinTreeNode i = listHead;
         while (i.next != null && prob > i.next.prob)
-        {
             i = i.next;
-        }
+
         return i;
     }
 
@@ -71,31 +122,223 @@ class HuffmanLinkedList
         System.out.print("listHead-->");
         for (listBinTreeNode i = listHead; i != null; i = i.next)
         {
-            if (i.next == null) nextString = "NULL)-->";
+            if (i.next == null) nextString = "null)-->";
             else                nextString = i.next.chStr + ")-->";
             System.out.print("(\"" + i.chStr + "\", " + i.prob + ", " + nextString);
         }
-        System.out.print("NULL\n");
+        System.out.print("null\n");
+    }
+
+    @Override
+    public String toString()
+    {
+        String nextString;
+        String result = "listHead-->";
+        for (listBinTreeNode i = listHead; i != null; i = i.next)
+        {
+            if (i.next == null) nextString = "null)-->";
+            else                 nextString = "\"" + i.next.chStr + "\")-->";
+            
+            result += "(\"" + i.chStr + "\", " + i.prob + ", " + nextString;
+        }
+        result += "null";
+        return result;
     }
 }
 
 class HuffmanBinaryTree 
 {
-    class charInfo
+    listBinTreeNode root;
+    
+    HuffmanBinaryTree() { root = null; }
+
+    void constructHuffmanBinTree(HuffmanLinkedList list, FileWriter output5)
     {
+        // Create a dummy node and oldListHead point to dummy node and save original linked list.
+        list.oldListHead = new listBinTreeNode("yummy", 0);
+        list.oldListHead.next = list.listHead.next;  
         
+        // Move listhead from "dummy" node to first node in list.
+        list.listHead = list.listHead.next; 
+        listBinTreeNode newNode = null;
+
+        try
+        {
+            output5.write("constructHuffmanBinTree(list, output5): \n");
+
+            // Keep going until there is one node in list. 
+            while (list.listHead.next != null) 
+            {
+                String combinedChar = list.listHead.chStr + list.listHead.next.chStr;
+                int combinedProb = list.listHead.prob + list.listHead.next.prob;
+
+                // Create newNode with combined prob & char
+                newNode = new listBinTreeNode(combinedChar, combinedProb); 
+
+                newNode.left = list.listHead; // Set newNode's left to first node of the list.
+                newNode.right = list.listHead.next; // Set newNode's right to second node of the list.
+
+                // Search and insert newNode starting from where listHead currently is.
+                list.listInsert(newNode.prob, newNode);   
+
+                // Print the new node for debugging.
+                listBinTreeNode.printNode(newNode);       
+                output5.write(newNode.toString() + '\n');
+
+                // Print the current list for debugging.
+                list.printList();                          
+                output5.write(list.toString() + '\n');
+
+                // Advance the current list head by two pointers.
+                list.listHead = list.listHead.next.next; 
+            }
+            // Set the root to the newly created HuffmanBinaryTree when there is only one node left.
+            root = newNode; 
+        }
+        catch (Exception e)
+        {
+            Main.printError(e);
+        }
     }
+
+    void preOrder(listBinTreeNode t, FileWriter output3)
+    {   
+        if (t != null)
+        {
+            listBinTreeNode.printNode(t);
+            try
+            { 
+                output3.write(t.toString() + '\n');
+            }
+            catch (Exception e)
+            {
+                Main.printError(e);
+            }
+            preOrder(t.left, output3);
+            preOrder(t.right, output3);
+        }
+    }
+
+    void inOrder(listBinTreeNode t, FileWriter output4)
+    {
+        if (t != null)
+        {
+            inOrder(t.left, output4);
+            listBinTreeNode.printNode(t);
+            try
+            { 
+                output4.write(t.toString() + '\n');
+            }
+            catch (Exception e)
+            {
+                Main.printError(e);
+            }
+            inOrder(t.right, output4);
+        }
+    }
+
+    void postOrder(listBinTreeNode t, FileWriter output5)
+    {
+        if (t != null)
+        {
+            postOrder(t.left, output5);
+            postOrder(t.right, output5);
+            listBinTreeNode.printNode(t);
+            try
+            { 
+                output5.write(t.toString() + '\n');
+            }
+            catch (Exception e)
+            {
+                Main.printError(e);
+            }
+        }
+    }
+
+    static boolean isLeaf(listBinTreeNode t) { return (t.left == null && t.right == null); }
+    
+        static void constructCharCode(listBinTreeNode t, String code, FileWriter output1)
+        {
+            try
+            {
+                if (t == null)
+                { 
+                    output1.write("This is an empty tree." + '\n');
+                    System.out.println("This is an empty tree." + '\n');
+                    System.exit(0); 
+                }
+                else if (HuffmanBinaryTree.isLeaf(t))
+                {
+                    t.code = code;
+                    output1.write(t.chStr + " : " + t.code + '\n');
+                    System.out.println(t.chStr + " : " + t.code + '\n');
+                }
+                else
+                {
+                    HuffmanBinaryTree.constructCharCode(t.left, code + "0", output1);
+                    HuffmanBinaryTree.constructCharCode(t.right, code + "1", output1);
+                }
+            }
+            catch (Exception e)
+            {
+                Main.printError(e);
+            }
+        }
 }
 
 public class Main
 {
     public static void main(String[] argv)
     {
-        HuffmanLinkedList hll = new HuffmanLinkedList();
-        hll.listInsert(10, new listBinTreeNode("a", 10));
-        hll.listInsert(10, new listBinTreeNode("b", 10));
-        hll.listInsert(10, new listBinTreeNode("c", 5));
+        try 
+        {
+            Scanner input = new Scanner(new FileReader(argv[0])); // Open input file.
+            FileWriter output1 = new FileWriter(argv[1]);   // For Huffman pairs.
+            FileWriter output2 = new FileWriter(argv[2]);   // For pre-order traversal of Huffman binary tree.
+            FileWriter output3 = new FileWriter(argv[3]);  // For in-order traversal of Huffman binary tree.
+            FileWriter output4 = new FileWriter(argv[4]);   // For post-order traversal of Huffman binary tree.
+            FileWriter output5 = new FileWriter(argv[5]);  // For debugging outputs.
+        
+            // Create Huffman linked list with input file.
+            HuffmanLinkedList hll = new HuffmanLinkedList(); 
+            hll.constructHuffmanList(input, output5); 
+        
+            // Create Huffman binary tree from linked list.
+            HuffmanBinaryTree hbt = new HuffmanBinaryTree();
+            hbt.constructHuffmanBinTree(hll, output5);      
+        
+            // Create the code for each char.
+            HuffmanBinaryTree.constructCharCode(hbt.root, "", output1);  
+        
+            output2.write("preOrder: \n");
+            output3.write("inOrder: \n");
+            output4.write("postOrder: \n");
+        
+            hbt.preOrder(hbt.root, output2); 
+            hbt.inOrder(hbt.root, output3);
+            hbt.postOrder(hbt.root, output4);
+        
+            input.close();
+            output1.close();
+            output2.close();
+            output3.close();
+            output4.close();
+            output5.close();
+        }
+        catch (Exception e)
+        {
+            printError(e);
+        }
+    }
 
-        hll.printList();
+    // For debugging purposes.
+    public static void printError(Exception e) 
+    {
+        StringWriter writer = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(writer);
+        e.printStackTrace(printWriter);
+        printWriter.flush();
+        String stackTrace = writer.toString();
+        System.out.println(stackTrace);
     }
 }
